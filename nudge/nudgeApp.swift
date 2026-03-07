cca5b315c695f4948897cca6f7c54ac7a4f014c5
Pranav Harshe <pranav.harshe94@gmail.com>
2026-03-07 20:10:00 +0530

    Initial Commit

---
+++ nudge/nudgeApp.swift
@@ -0,0 +1,32 @@
+//
+//  nudgeApp.swift
+//  nudge
+//
+//  Created by Pranav Harshe on 07/03/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct nudgeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
